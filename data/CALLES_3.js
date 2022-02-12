876ef4184d4c15f9ad7aaa331ef99e0f490a8b32
FelipeGalloZarza <plancpure@gmail.com>
2022-02-11 21:53:50 -0600

    primera edicion

---
+++ data/CALLES_3.js
@@ -0,0 +1,11 @@
+var json_CALLES_3 = {
+"type": "FeatureCollection",
+"name": "CALLES_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Calle": "CALLE ROBLES" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -99.464961597000567, 19.352617972369142 ], [ -99.463482281718257, 19.352495503597712 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Calle": "CALLE ENCINO" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -99.464293893743687, 19.352587574391464 ], [ -99.464291939387522, 19.352880925170421 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Calle": "CALLE ABETO" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -99.464177800380241, 19.352231098326573 ], [ -99.464135773245033, 19.351810126421647 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Calle": "ACCESO SECUNDARIO" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -99.4654682722188, 19.353029440969664 ], [ -99.463652443354448, 19.352870221188031 ] ] ] } }
+]
+}
